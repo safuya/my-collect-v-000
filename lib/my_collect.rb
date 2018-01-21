bb38e95159777387491b12c49d87e850a65623f5
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-21 08:51:53 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  collection = []
+  if block_given?
+    counter = 0
+    while counter < array.length
+      collection << yield(array[counter])
+      counter += 1
+    end
+  end
+  collection
+end
